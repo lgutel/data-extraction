18e1f592be2d140d68461a9a76d727283683d582
lgutel <150175199+lgutel@users.noreply.github.com>
2024-05-31 11:51:33 +0200

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -1,11 +1,8 @@
 import mysql.connector
-from mysql.connector import errorcode
 import csv
 import os
-import pandas as pd
-
-
-def connexionsql(choix, chemin_fichier_csv):
+from influxdb import InfluxDBClient
+def connexionMySQL_MariaDB(choix, chemin_fichier_csv):
     db = mysql.connector.connect(
         host=input("Entrez l'@ip de votre serveur: "),
         user=input("Entrez votre login: "),
@@ -83,27 +80,35 @@ def connexionsql(choix, chemin_fichier_csv):
             while True:
                 print(
                     "La fonction de sauvegarde des anciennes tables ne fonctionne qu'à moitié. Je vous propose d'enregistrer vos tables directement sur place. Merci :/")
-                b = input("Voulez-vous sauvegarder les anciennes tables (oui/non/quitter): ")
-                if b.lower() == "oui":
-                    tables_to_export = input("Entrez les noms des tables à exporter, séparés par des virgules: ").split(
-                        ',')
-                    for table in tables_to_export:
-                        file_name = f"{table.strip()}.csv"
-                        try:
-                            query = f"SELECT * FROM {table.strip()}"
-                            cursor.execute(query)
-                            rows = cursor.fetchall()
-                            columns = [i[0] for i in cursor.description]
-                            with open(file_name, mode='w', newline='') as file:
-                                writer = csv.writer(file)
-                                writer.writerow(columns)
-                                writer.writerows(rows)
-                            print(
-                                f"Les données de la table {table.strip()} ont été exportées avec succès dans le fichier {file_name}.")
-                        except mysql.connector.Error as err:
-                            print(f"Erreur: {err}")
-
-                elif b.lower() == "non":
+                b = input("Voulez-vous supprimer les anciennes tables (oui/non/quitter): ")
+                if b.lower() == "non":
+                    try:
+                        chemin_fichier_csv = rf"{chemin_fichier_csv}\{choix}"
+                        ## Compter le nombre de colonnes pour créer la table
+                        with open(chemin_fichier_csv, newline='') as csvfile:
+                            reader = csv.reader(csvfile)
+                            num_columns = len(next(reader))
+                        ## Créer la requête pour créer la table
+                        table_name = os.path.splitext(os.path.basename(chemin_fichier_csv))[0]
+                        create_table_query = f"CREATE TABLE IF NOT EXISTS `{table_name}` ("
+                        for i in range(num_columns):
+                            create_table_query += f"colonne_{i} VARCHAR(255), "
+                        create_table_query = create_table_query.rstrip(', ') + ")"
+                        cursor.execute(create_table_query)
+                        db.commit()
+                        print(f"La table `{table_name}` a été créée avec succès.")
+                        ## Insérer les données du CSV dans la table
+                        with open(chemin_fichier_csv, newline='') as csvfile:
+                            csv_data = csv.reader(csvfile)
+                            next(csv_data)  # Skip header
+                            for row in csv_data:
+                                cursor.execute(f"INSERT INTO `{table_name}` VALUES ({', '.join(['%s'] * num_columns)})",
+                                               row)
+                            db.commit()
+                            print(f"Données importées avec succès dans la table `{table_name}`.")
+                    except mysql.connector.Error as err:
+                        print(f"Erreur: {err}")
+                elif b.lower() == "oui":
                     try:
                         cursor.execute("SET FOREIGN_KEY_CHECKS = 0;")
                         cursor.execute(
@@ -146,5 +151,53 @@ def connexionsql(choix, chemin_fichier_csv):
         elif a.lower() == "quitter":
             cursor.close()
             db.close()
+            print("Vous avez quitté avec succès")
+            break
+
+def connexionInfluxDB(choix, chemin_fichier_csv):
+    client = InfluxDBClient(host=input("Entrez l'adresse IP de votre serveur InfluxDB: "),
+                            port=input("Entrez le port de votre serveur InfluxDB (par défaut 8086): "),
+                            username=input("Entrez votre nom d'utilisateur InfluxDB: "),
+                            password=input("Entrez votre mot de passe InfluxDB: "),
+                            database=input("Entrez le nom de votre base de données InfluxDB: "))
+    while True:
+        print("L'ajout manuel de mesure n'est pas pris en charge dans InfluxDB.")
+        a = input("Voulez-vous passer en mode manuel ou automatique? (auto/manu/quitter): ")
+        if a.lower() == "manu":
+            while True:
+                b = input("Voulez-vous lire ou quitter: ")
+                if b.lower() == "lire":
+                    mesure = input("Entrez le nom de votre mesure: ")
+                    query = f"SELECT * FROM {mesure}"
+                    resultats = client.query(query)
+                    for resultat in resultats.get_points():
+                        print(resultat)
+                elif b.lower() == "quitter":
+                    break
+
+        elif a.lower() == "auto" and choix is not None:
+            while True:
+                print("La sauvegarde et la suppression des anciennes mesures n'est pas pris en charge dans InfluxDB.")
+                b = input("Voulez-vous ajouter de nouvelle mesures (oui/non): ")
+                if b.lower() == "oui":
+                    try:
+                        chemin_fichier_csv = rf"{chemin_fichier_csv}\{choix}"
+                        nom_mesure = os.path.splitext(os.path.basename(chemin_fichier_csv))[0]
+                        with open(chemin_fichier_csv, newline='') as csvfile:
+                            csv_data = csv.reader(csvfile)
+                            next(csv_data)  # Skip header
+                            for row in csv_data:
+                                data = [{
+                                    "measurement": nom_mesure,
+                                    "fields": dict(enumerate(row))
+                                }]
+                                client.write_points(data)
+                            print(f"Données importées avec succès dans la mesure '{nom_mesure}'.")
+                    except Exception as e:
+                        print(f"Erreur: {e}")
+                elif b.lower() == "non":
+                    break
+        elif a.lower() == "quitter":
+            client.close()
             print("Vous avez quitté avec succès")
             break
